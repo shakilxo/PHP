ffc22e7580be53528db52c556f82bd4e1c69c5ae
shakilxo <shakilinfo91@gmail.com>
2025-05-31 15:40:35 +0600

    up

--- syntax.php
+++ syntax.php
@@ -6,6 +6,9 @@
     <?php
         echo "My first PHP script!";
     ?>
+    <h3>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Qui ratione beatae nihil nostrum odio omnis officia
+        aperiam a inventore veniam. Magnam, impedit cupiditate? Nisi deleniti obcaecati dolores quia ratione maiores?
+    </h3>
 </body>
 
 </html>
